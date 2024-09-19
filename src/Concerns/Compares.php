76555f54facc56aebc10282689ebeb52fb09b3df
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2024-09-19 17:10:42 -0300

    Improve docblocks

--- src/Concerns/Compares.php
+++ src/Concerns/Compares.php
@@ -3,16 +3,12 @@
 namespace Cerbero\Enum\Concerns;
 
 /**
- * The trait to compare cases of an enum.
- *
+ * The trait to compare the cases of an enum.
  */
 trait Compares
 {
     /**
-     * Determine whether the enum has the given target
-     *
-     * @param mixed $target
-     * @return bool
+     * Determine whether the enum includes the given target.
      */
     public static function has(mixed $target): bool
     {
@@ -26,10 +22,7 @@ trait Compares
     }
 
     /**
-     * Determine whether the enum does not have the given target
-     *
-     * @param mixed $target
-     * @return bool
+     * Determine whether the enum does not include the given target.
      */
     public static function doesntHave(mixed $target): bool
     {
@@ -43,10 +36,7 @@ trait Compares
     }
 
     /**
-     * Determine whether the current case matches the given target
-     *
-     * @param mixed $target
-     * @return bool
+     * Determine whether this case matches the given target.
      */
     public function is(mixed $target): bool
     {
@@ -54,10 +44,7 @@ trait Compares
     }
 
     /**
-     * Determine whether the current case does not match the given target
-     *
-     * @param mixed $target
-     * @return bool
+     * Determine whether this case does not match the given target.
      */
     public function isNot(mixed $target): bool
     {
@@ -65,10 +52,9 @@ trait Compares
     }
 
     /**
-     * Determine whether the current case matches at least one of the given targets
+     * Determine whether this case matches at least one of the given targets.
      *
-     * @param iterable $targets
-     * @return bool
+     * @param iterable<array-key, mixed> $targets
      */
     public function in(iterable $targets): bool
     {
@@ -82,10 +68,9 @@ trait Compares
     }
 
     /**
-     * Determine whether the current case does not match any of the given targets
+     * Determine whether this case does not match any of the given targets.
      *
-     * @param iterable $targets
-     * @return bool
+     * @param iterable<array-key, mixed> $targets
      */
     public function notIn(iterable $targets): bool
     {
